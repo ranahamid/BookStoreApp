dae131f53dc954bf061765e009f8389a286cd37d
Rana Hamid <rhamid@mymedicalhub.com>
2022-04-07 23:02:23 +0600

    automapper isssue in author

--- BookStoreApp.Blazor.Server.UI/Configurations/MapperConfig.cs
+++ BookStoreApp.Blazor.Server.UI/Configurations/MapperConfig.cs
@@ -3,12 +3,11 @@ using BookStoreApp.Blazor.Server.UI.Services.Base;
 
 namespace BookStoreApp.Blazor.Server.UI.Configurations
 {
-    public class MapperConfig:Profile
+    public class MapperConfig : Profile
     {
         public MapperConfig()
         {
             CreateMap<AuthorReadOnlyDto, AuthorUpdateDto>().ReverseMap();
-            
         }
     }
 }
